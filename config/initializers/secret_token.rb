521261eab16298fd45446686f097c78deeefa612
Tiffany Wallace <enyrgie@gmail.com>
2013-08-02 23:19:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,27 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+LoungeApp::Application.config.secret_key_base = '502f780097c1ebf05110ae56f3c729df313a5424273099a9d36e510ff25469c56e9748e264f11d8491ca64b0c4cf2f7aefab78390af100ea2b0700d80ce017a7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LoungeApp::Application.config.session_store :cookie_store, key: '_lounge_app_session'
