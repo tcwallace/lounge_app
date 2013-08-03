51b0356520b12c3a11a72ebb1d97ad4fe038a1c9
Tiffany Wallace <enyrgie@gmail.com>
2013-08-03 01:00:16 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,20 +2,27 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { "London's Lounge" }
+
   describe "Home page" do
 
-    it "should have the content 'London's Lounge'" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content("London's Lounge")
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("London's Lounge | Home")
+      expect(page).to have_title("London's Lounge")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
-   describe "Help page" do
+  describe "Help page" do
 
     it "should have the content 'Help'" do
       visit '/static_pages/help'
@@ -24,11 +31,11 @@ describe "Static pages" do
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title("London's Lounge | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
 
-   describe "About page" do
+  describe "About page" do
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
@@ -37,7 +44,20 @@ describe "Static pages" do
 
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title("London's Lounge | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
     end
   end
 end
